3791a257825a720b43d851e93092693420970958
Dystian En <distian.enyus@gmail.com>
2023-03-19 16:46:23 +0700

    chore layout

--- src/app/landing-page/page.tsx
+++ src/app/landing-page/page.tsx
@@ -1,12 +1,15 @@
-"use client"
+'use client';
 import React from 'react';
-import Register from "@/components/register/page";
-import { Box } from "@chakra-ui/react";
+import Register from '@/components/register/page';
+import { Box} from '@chakra-ui/react';
 
 export default function LandingPage() {
   return (
-    <Box h='100vh' display='flex' alignItems='center' justifyContent='center'>
-      <Register />
+    <Box minH='100vh' bgColor={'#E3EE'} display={'flex'} flexDirection='column' alignItems={'center'}>
+    {/* <Box minH='100vh' w='100vw' bgColor={'#E3EEFF'} display={'flex'} justifyContent={'center'}> */}
+      {/* <Box display={'flex'} flexDirection='column' alignItems={'center'}> */}
+        <Register />
+      {/* </Box> */}
     </Box>
-  )
+  );
 }
--- src/components/register/page.tsx
+++ src/components/register/page.tsx
@@ -1,77 +1,70 @@
-"use client"
+'use client';
 import React from 'react';
-import {
-  Text,
-  Flex,
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  FormHelperText,
-  Input, Box,
-  Center, Select
-} from '@chakra-ui/react'
+import { Text, Flex, FormControl, FormLabel, FormErrorMessage, FormHelperText, Input, Box, Center, Select, Grid, Button, HStack, Spacer, ButtonGroup } from '@chakra-ui/react';
 
 export default function Register() {
   return (
-    <Box display={'grid'} placeItems={'center'} minWidth={'50%'}>
-      <Text fontSize='40px' align='center' mb='15px'>Daftarkan Dirimu Sekarang</Text>
+    <>
+      <Text fontSize='40px' align='center' mb='15px' color='#000000' fontWeight='600'>
+        Daftarkan Dirimu Sekarang
+      </Text>
 
-      <Box
-        w={'100%'}
-        m={'15px'}
-        p={'15px'}
-        px={{base: '15px', md: '10rem'}}
-        bgColor={'lightgray'}
-        borderRadius={'15px'}
-        display={'flex'}
-        flexDirection={'column'}
-        gap={'15px'}
-        bgGradient='linear-gradient(180deg, #EBF0F7 -181.05%, rgba(255, 255, 255, 0.21) 343.51%)'
-      >
-        <FormControl>
-          <FormLabel>Akun Kelas Pintar</FormLabel>
-          <Input isRequired={true} bgColor={'white'} focusBorderColor='grey' bgGradient='linear-gradient(180deg, #EBF0F7 -181.05%, rgba(255, 255, 255, 0.21) 343.51%)'  type='email'/>
-        </FormControl>
+      <Box w={'922px'} py={'15px'} px={'10rem'} bgColor={'lightgray'} borderRadius={'15px'} display={'flex'} justifyContent={'center'} gap={'15px'} bgGradient='linear-gradient(180deg, #EBF0F7 -181.05%, rgba(255, 255, 255, 0.21) 343.51%)'>
+        <Grid gap={'15px'}>
+          <FormControl>
+            <FormLabel color={'#000000'}>Akun Kelas Pintar</FormLabel>
+            <Input isRequired={true} bgColor={'white'} type='email' focusBorderColor='grey' borderRadius='30px' />
+          </FormControl>
 
-        <FormControl>
-          <FormLabel>Nama Lengkap</FormLabel>
-          <Input bgColor={'white'} focusBorderColor='grey' bgGradient='linear-gradient(180deg, #EBF0F7 -181.05%, rgba(255, 255, 255, 0.21) 343.51%)'/>
-        </FormControl>
+          <FormControl>
+            <FormLabel color={'#000000'}>Nama Lengkap</FormLabel>
+            <Input bgColor={'white'} focusBorderColor='grey' borderRadius='30px' />
+          </FormControl>
 
-        <FormControl>
-          <FormLabel>Nama Sekolah</FormLabel>
-          <Select bgColor={'white'} focusBorderColor='grey' bgGradient='linear-gradient(180deg, #EBF0F7 -181.05%, rgba(255, 255, 255, 0.21) 343.51%)' placeholder='Select option'>
-            <option value='option1'>Option 1</option>
-            <option value='option2'>Option 2</option>
-            <option value='option3'>Option 3</option>
-          </Select>
-        </FormControl>
+          <FormControl>
+            <FormLabel color={'#000000'}>Nama Sekolah</FormLabel>
+            <Select bgColor={'white'} color={'#000000'} focusBorderColor='grey' borderRadius='30px' placeholder='Select option'>
+              <option value='option1'>Option 1</option>
+              <option value='option2'>Option 2</option>
+              <option value='option3'>Option 3</option>
+            </Select>
+          </FormControl>
 
-        <FormControl>
-          <FormLabel>Kelas</FormLabel>
-          <Select bgColor={'white'} focusBorderColor='grey' bgGradient='linear-gradient(180deg, #EBF0F7 -181.05%, rgba(255, 255, 255, 0.21) 343.51%)' placeholder='Select option'>
-            <option value='option1'>Option 1</option>
-            <option value='option2'>Option 2</option>
-            <option value='option3'>Option 3</option>
-          </Select>
-        </FormControl>
+          <FormControl>
+            <FormLabel color={'#000000'}>Kelas</FormLabel>
+            <Select bgColor={'white'} focusBorderColor='grey' borderRadius='30px' placeholder='Select option'>
+              <option value='option1'>Option 1</option>
+              <option value='option2'>Option 2</option>
+              <option value='option3'>Option 3</option>
+            </Select>
+          </FormControl>
 
-        <FormControl>
-          <FormLabel>Nomor Handphone ( *yang terhubung di WhatsApp )</FormLabel>
-          <Input bgColor={'white'} focusBorderColor='grey' bgGradient='linear-gradient(180deg, #EBF0F7 -181.05%, rgba(255, 255, 255, 0.21) 343.51%)'/>
-        </FormControl>
+          <FormControl>
+            <FormLabel color={'#000000'}>Nomor Handphone ( *yang terhubung di WhatsApp )</FormLabel>
+            <Input bgColor={'white'} focusBorderColor='grey' borderRadius='30px' />
+          </FormControl>
 
-        <FormControl>
-          <FormLabel>Nomor Handphone Orang Tua</FormLabel>
-          <Input bgColor={'white'} focusBorderColor='grey' bgGradient='linear-gradient(180deg, #EBF0F7 -181.05%, rgba(255, 255, 255, 0.21) 343.51%)'/>
-        </FormControl>
+          <FormControl>
+            <FormLabel color={'#000000'}>Nomor Handphone Orang Tua</FormLabel>
+            <Input bgColor={'white'} focusBorderColor='grey' borderRadius='30px' />
+          </FormControl>
 
-        <FormControl>
-          <FormLabel>Buat Kata Sandi</FormLabel>
-          <Input bgColor={'white'} focusBorderColor='grey' bgGradient='linear-gradient(180deg, #EBF0F7 -181.05%, rgba(255, 255, 255, 0.21) 343.51%)'/>
-          <FormHelperText>*Minimal 6 Karakter</FormHelperText>
-        </FormControl>
+          <FormControl>
+            <FormLabel color={'#000000'}>Buat Kata Sandi</FormLabel>
+            <Input bgColor={'white'} focusBorderColor='grey' borderRadius='30px' />
+            <FormHelperText>*Minimal 6 Karakter</FormHelperText>
+          </FormControl>
+
+          <ButtonGroup spacing='2'>
+            <Button w={'235px'} variant='outline' colorScheme='blue' borderRadius={'30px'}>
+              Reset
+            </Button>
+            <Button w={'235px'} variant='solid' colorScheme='blue' borderRadius={'30px'}>
+              Daftar
+            </Button>
+          </ButtonGroup>
+        </Grid>
       </Box>
-    </Box>
-  )
+    </>
+  );
 }
