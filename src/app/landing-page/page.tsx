fcf014a247aefa7937f5be59eae0127d0f8a223a
Dystian En <dystian.en@kelaspintar.co.id>
2023-03-19 20:05:43 +0700

    responsive form

--- src/app/landing-page/page.tsx
+++ src/app/landing-page/page.tsx
@@ -1,15 +1,12 @@
 'use client';
 import React from 'react';
 import Register from '@/components/register/page';
-import { Box} from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 
 export default function LandingPage() {
   return (
-    <Box minH='100vh' bgColor={'#E3EE'} display={'flex'} flexDirection='column' alignItems={'center'}>
-    {/* <Box minH='100vh' w='100vw' bgColor={'#E3EEFF'} display={'flex'} justifyContent={'center'}> */}
-      {/* <Box display={'flex'} flexDirection='column' alignItems={'center'}> */}
-        <Register />
-      {/* </Box> */}
+    <Box minH='100vh' bgColor={'#E3EEFF'} display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
+      <Register />
     </Box>
   );
 }
--- src/components/register/page.tsx
+++ src/components/register/page.tsx
@@ -1,37 +1,39 @@
 'use client';
 import React from 'react';
-import { Text, Flex, FormControl, FormLabel, FormErrorMessage, FormHelperText, Input, Box, Center, Select, Grid, Button, HStack, Spacer, ButtonGroup } from '@chakra-ui/react';
+import { Text, FormControl, FormLabel, FormHelperText, Input, Box, Select, Grid, Button, ButtonGroup } from '@chakra-ui/react';
 
 export default function Register() {
   return (
-    <>
-      <Text fontSize='40px' align='center' mb='15px' color='#000000' fontWeight='600'>
+    <Box m={'20px'}>
+      <Text fontSize={{ base: '24px', lg: '40px' }} align='center' color='#000000' fontWeight='600'>
         Daftarkan Dirimu Sekarang
       </Text>
 
-      <Box w={'922px'} py={'15px'} px={'10rem'} bgColor={'lightgray'} borderRadius={'15px'} display={'flex'} justifyContent={'center'} gap={'15px'} bgGradient='linear-gradient(180deg, #EBF0F7 -181.05%, rgba(255, 255, 255, 0.21) 343.51%)'>
+      <Box
+        m={'20px'}
+        py={'1rem'}
+        px={{ base: '1rem', sm: '4rem', md: '5rem', lg: '10rem' }}
+        bgColor={'lightgray'}
+        borderRadius={'15px'}
+        display={'flex'}
+        justifyContent={'center'}
+        gap={'15px'}
+        bgGradient='linear-gradient(180deg, #EBF0F7 -181.05%, rgba(255, 255, 255, 0.21) 343.51%)'
+        backdropFilter='blur(25px)'
+      >
         <Grid gap={'15px'}>
           <FormControl>
-            <FormLabel color={'#000000'}>Akun Kelas Pintar</FormLabel>
+            <FormLabel>Akun Kelas Pintar</FormLabel>
             <Input isRequired={true} bgColor={'white'} type='email' focusBorderColor='grey' borderRadius='30px' />
           </FormControl>
 
           <FormControl>
-            <FormLabel color={'#000000'}>Nama Lengkap</FormLabel>
+            <FormLabel>Nama Lengkap</FormLabel>
             <Input bgColor={'white'} focusBorderColor='grey' borderRadius='30px' />
           </FormControl>
 
           <FormControl>
-            <FormLabel color={'#000000'}>Nama Sekolah</FormLabel>
-            <Select bgColor={'white'} color={'#000000'} focusBorderColor='grey' borderRadius='30px' placeholder='Select option'>
-              <option value='option1'>Option 1</option>
-              <option value='option2'>Option 2</option>
-              <option value='option3'>Option 3</option>
-            </Select>
-          </FormControl>
-
-          <FormControl>
-            <FormLabel color={'#000000'}>Kelas</FormLabel>
+            <FormLabel>Nama Sekolah</FormLabel>
             <Select bgColor={'white'} focusBorderColor='grey' borderRadius='30px' placeholder='Select option'>
               <option value='option1'>Option 1</option>
               <option value='option2'>Option 2</option>
@@ -40,31 +42,40 @@ export default function Register() {
           </FormControl>
 
           <FormControl>
-            <FormLabel color={'#000000'}>Nomor Handphone ( *yang terhubung di WhatsApp )</FormLabel>
+            <FormLabel>Kelas</FormLabel>
+            <Select bgColor={'white'} focusBorderColor='grey' borderRadius='30px' placeholder='Select option'>
+              <option value='option1'>Option 1</option>
+              <option value='option2'>Option 2</option>
+              <option value='option3'>Option 3</option>
+            </Select>
+          </FormControl>
+
+          <FormControl>
+            <FormLabel>Nomor Handphone ( *yang terhubung di WhatsApp )</FormLabel>
             <Input bgColor={'white'} focusBorderColor='grey' borderRadius='30px' />
           </FormControl>
 
           <FormControl>
-            <FormLabel color={'#000000'}>Nomor Handphone Orang Tua</FormLabel>
+            <FormLabel>Nomor Handphone Orang Tua</FormLabel>
             <Input bgColor={'white'} focusBorderColor='grey' borderRadius='30px' />
           </FormControl>
 
           <FormControl>
-            <FormLabel color={'#000000'}>Buat Kata Sandi</FormLabel>
+            <FormLabel>Buat Kata Sandi</FormLabel>
             <Input bgColor={'white'} focusBorderColor='grey' borderRadius='30px' />
             <FormHelperText>*Minimal 6 Karakter</FormHelperText>
           </FormControl>
 
           <ButtonGroup spacing='2'>
-            <Button w={'235px'} variant='outline' colorScheme='blue' borderRadius={'30px'}>
+            <Button w={{ base: '150px', lg: '235px' }} variant='outline' colorScheme='blue' borderRadius={'30px'}>
               Reset
             </Button>
-            <Button w={'235px'} variant='solid' colorScheme='blue' borderRadius={'30px'}>
+            <Button w={{ base: '150px', lg: '235px' }} variant='solid' colorScheme='blue' borderRadius={'30px'}>
               Daftar
             </Button>
           </ButtonGroup>
         </Grid>
       </Box>
-    </>
+    </Box>
   );
 }
